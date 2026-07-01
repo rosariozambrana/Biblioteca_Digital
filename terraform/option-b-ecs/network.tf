bb0244591bbf34b535c219d41c04eb391f80c19e
rexy1818 <ramirojordanjurado@gmail.com>
2026-07-01 15:02:03 -0400

    implementacion de terraform y ci/cd

--- terraform/option-b-ecs/network.tf
+++ terraform/option-b-ecs/network.tf
@@ -27,3 +27,27 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
+
+# Implementado: NAT Gateway para salida a Internet desde subnets privadas
+# Se crea en una subnet pública y se asocia con la route table privada
+resource "aws_eip" "nat" {
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "${var.project_name}-nat-gw"
+  }
+}
+
+# Ruta en la tabla privada hacia el NAT Gateway
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
+}
+
